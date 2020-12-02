5b5ec1d2aa292aabbc054f64edd494a3cf51eb94
milkybar1910 <sivashankar1326@gmail.com>
2020-12-02 14:51:03 +0530

    DARK MODE || WHITE MODE

---
+++ five-theme-switcher/src/Colours.js
@@ -0,0 +1,14 @@
+const AppTheme ={
+    light:{
+        textColor:"#000000",
+        backgroundColor:"#FFFFFF",
+        border:"2px solid #000000"
+    },
+    dark:{
+        textColor:"#FFFFFF",
+        backgroundColor:"#333333",
+        border:"2px dashed #000000"
+    }
+}
+
+export default AppTheme;
---
+++ five-theme-switcher/src/Components/HeroSection.js
@@ -0,0 +1,36 @@
+import React, { useContext } from "react"
+import ThemeContext from "../Context/ThemeContext"
+import AppTheme from "../Colours"
+
+const HeroSection =() =>{
+    const theme =useContext(ThemeContext)[0]
+    const currentTheme = AppTheme[theme]
+
+    return (
+        
+        <div 
+            style={{
+                padding:"1rem",
+                backgroundColor:`${currentTheme.backgroundColor}`,
+                color: `${currentTheme.textColor}`,
+                textAlign:"center"
+            }}
+        >
+            <h1>Context API theme toggler</h1>
+            <p>This is a nice paragraph</p>
+            <button 
+                style={{
+                    backgroundColor:"#26ae60",
+                    padding:"10px 150px",
+                    fontSize:"20px",
+                    color:"white",
+                    border:`${currentTheme.border}`
+                }}
+            >Click me</button>
+            { console.log(theme,currentTheme)}
+        </div>
+       
+    )
+}
+
+export default HeroSection;
